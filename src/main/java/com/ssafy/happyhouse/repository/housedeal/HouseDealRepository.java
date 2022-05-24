388034cac981d746c859cc91f0270a524012b7e4
minjuukim <kminju919@gmail.com>
2022-05-24 22:35:48 +0900

    MODIFY: housedealrepository filename change

--- src/main/java/com/ssafy/happyhouse/repository/housedeal/HouseDealRepository.java
+++ src/main/java/com/ssafy/happyhouse/repository/housedeal/HouseDealRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface HouseHouseDealRepository
+public interface HouseDealRepository
     extends JpaRepository<HouseDeal, Long>, HouseDealRepositoryCustom { 
 	
 	@Query("SELECT d FROM HouseDeal d WHERE house_id = :houseId ORDER BY deal_date DESC")
